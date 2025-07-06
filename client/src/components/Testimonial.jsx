6a1078153c98b943b5c1c719672e226422d63131
Nandita0102 <nandita.thakur.work@gmail.com>
2025-07-06 21:27:38 +0530

    first commit

---
+++ client/src/components/Testimonial.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Title from './Title'
+import { assets } from '../assets/assets';
+import {motion} from 'motion/react'
+
+const Testimonial = () => {
+    const testimonials = [
+        { name: "Priya Varrier", 
+          location: "Jabalpur, Madhya Pradesh",
+          image: assets.testimonial_image_1, 
+          testimonial: "I booked a Lamborghini Huracán for my birthday through Vroom, and it was worth every penny! The car arrived spotless, with a handwritten note. Felt like a celebrity!" 
+        },
+        { name: "Neeraja Dixit", 
+          location: "Jabalpur, Madhya Pradesh",
+          image: assets.testimonial_image_2, 
+          testimonial: "I thought luxury rentals were all hype until I tried Vroom. Now I’m hooked—their insurance transparency and no-hidden-fees policy are just as impressive as their fleet." 
+        },
+        { name: "Gracie Rodriguez", 
+          location: "Indore, Madhya Pradesh",
+          image: assets.testimonial_image_1, 
+          testimonial: "As a frequent traveler, I need reliability and style. Vroom delivers both—whether it’s a Mercedes S-Class for client meetings or a Range Rover for weekend getaways. The app’s VIP support saves me hours." 
+        }
+        
+    ];
+  return (
+    
+    <div className="py-28 px-6 md:px-16 lg:px-24 xl:px-44">
+
+        <Title title= "What Our Customers Say" subTitle="Discover why wise travellers choose Vroom for their luxury tarvels around the world." />
+            
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
+                {testimonials.map((testimonial,index) => (
+                    <motion.div 
+                    initial={{ opacity: 0, y: 40 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, delay: index * 0.2, ease: 'easeOut' }}
+                    viewport={{ once: true, amount: 0.3 }}
+                                                        
+                    key={index} className="bg-white p-6 rounded-xl shadow-lg hover:translate-y-1 transition-all duration-500">
+                        <div className="flex items-center gap-3">
+                            <img className="w-12 h-12 rounded-full" src={testimonial.image} alt={testimonial.name} />
+                            <div>
+                                <p className="text-xl">{testimonial.name}</p>
+                                <p className="text-gray-500">{testimonial.location}</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-1 mt-4">
+                            {Array(5).fill(0).map((_, index) => (
+                                <img key={index} src={assets.star_icon} alt="star-icon" />
+                                
+                            ))}
+                        </div>
+                        <p className="text-gray-500 max-w-90 mt-4 font-light">"{testimonial.testimonial}"</p>
+                    </motion.div>
+                ))}
+            </div>
+        </div>
+  )
+}
+
+export default Testimonial
---
+++ server/controllers/ownerController.js
@@ -0,0 +1,190 @@
+import imagekit from "../configs/imagekit.js";
+import Booking from "../models/Booking.js";
+import Car from "../models/Car.js";
+import User from "../models/user.js";
+import fs from "fs";
+//api to change role of user
+export const changeRoleToOwner = async (req, res)=>{
+    try {
+        const {_id} = req.user;
+        await User.findByIdAndUpdate(_id,{role: "owner"})
+        res.json({success:true, message: "Now you can list cars"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: "error.message"})
+    }
+
+}
+
+//api to list car
+
+export const addCar = async(req , res)=>{
+    try {
+        const {_id} = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+
+        const fileBuffer = fs.readFileSync(imageFile.path)//upload image to imagekit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        })
+        // For URL Generation, works for both images and videos
+        var optimizedImageURL = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width : '1280'}, //width resizing
+                {quality : 'auto'}, //auto compression
+                {format : 'webp'} //convert to modern format 
+            ]
+        });
+
+        const image = optimizedImageURL;
+        await Car.create({...car, owner: _id, image})
+
+        res.json({success:true, message:"Car Added"})
+
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: "error.message"})
+    }
+}
+
+//api to list owner cars
+
+export const getOwnerCars = async(req, res)=>{
+    try {
+        const {_id} = req.user;
+        const cars = await Car.find({owner: _id})
+        res.json({success:true, cars})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: "error.message"})
+    }
+}
+
+//api to toggle car availability
+
+export const toggleCarAvailability = async(req,res) =>{
+    // console.log(req?.user?._id)
+    // console.log("ToggleCaravilability is called")
+    
+    try {
+        const {_id} = req.user;
+        const {carId} = req.body
+        const car = await Car.findById(carId)
+        // console.log(car)
+        
+
+        //checking if car belongs to user
+        if(car?.owner?.toString()!==_id.toString()){
+            return res.json({success:false, message: "Unauthorized"})
+        }
+        car.isAvailable = !car.isAvailable;
+        await car.save()
+
+        res.json({success:true, message: "Availability Toggled"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: "error.message"})
+    }
+}
+
+//api to delete a car
+
+export const deleteCar = async(req,res) =>{
+    try {
+        const {_id} = req.user;
+        const {carId} = req.body
+        const car = await Car.findById(carId)
+
+        //checking if car belongs to user
+        if(car.owner.toString()!==_id.toString()){
+            return res.json({success:false, message: "Unauthorized"})
+        }
+        car.owner = null;
+        car.isAvailable = false;
+        await car.save()
+
+        res.json({success:true, message: "Car Removed"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: "error.message"})
+    }
+}
+
+//api to get dashboard data
+export const getDashboardData = async (req,res) =>{
+    try {
+        const {_id,role} = req.user;
+
+        if(role !== 'owner'){
+            return res.json({success:false, message: "Unauthorized"})
+        
+        }
+
+        const cars = await Car.find({owner:_id})
+        const bookings = await Booking.find({owner: _id}).populate('car').sort({createdAt: -1})
+
+        const pendingBookings = await Booking.find({owner: _id, status: "pending"})
+        const completedBookings = await Booking.find({owner: _id, status: "confirmed"})
+
+        //calculate monthly revenue from bookings where status is confirmed
+        // Calculate monthly revenue from confirmed bookings
+        const monthlyRevenue = bookings
+        .filter(booking => booking.status === 'confirmed')
+        .reduce((acc, booking) => acc + booking.price, 0);
+
+        const dashboardData = {
+            totalCars: cars.length,
+            totalBookings: bookings.length,
+            pendingBookings: pendingBookings.length,
+            completedBookings: completedBookings.length,
+            recentBookings: bookings.slice(0,3),
+            monthlyRevenue
+        }
+
+        res.json({ success: true, dashboardData });
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: "error.message"})
+    }
+}
+
+//API to update user image
+
+export const updateUserImage = async(req, res) =>{
+    try {
+        const {_id} = req.user;
+
+        const imageFile = req.file;
+
+        const fileBuffer = fs.readFileSync(imageFile.path)//upload image to imagekit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/users'
+        })
+        // For URL Generation, works for both images and videos
+        var optimizedImageURL = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width : '400'}, //width resizing
+                {quality : 'auto'}, //auto compression
+                {format : 'webp'} //convert to modern format 
+            ]
+        });
+
+        const image = optimizedImageURL;
+        await User.findByIdAndUpdate(_id, {image});
+        res.json({success:true, message: "Image Updated"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message: "error.message"})
+    }
+}
